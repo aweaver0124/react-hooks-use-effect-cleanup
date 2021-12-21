0b583959c49474b24fd9eae01d5b36f7599f8fd6
Alexandra Weaver <aweaver0124@gmail.com>
2021-12-21 17:00:41 -0600

    completed

--- src/Clock.js
+++ src/Clock.js
@@ -4,12 +4,17 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const timerID = setInterval(() => {
       setTime(new Date());
     }, 1000);
-  }, []);
 
-  return <div>{time.toString()}</div>;
-}
+    return function cleanip() {
+      clearInterval(timerID)
+    }
+   }, []);
+
+ return <div>{time.toString()}</div>
+  }
+ 
 
 export default Clock;
